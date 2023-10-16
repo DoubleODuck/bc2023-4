c51e9180bd66cdd14c6d4529f6394ec3c74a58da
Mykola <nikomyk79@gmail.com>
2023-10-16 18:38:16 +0300

    made it work

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
 const xml = require("fast-xml-parser");
 const fs = require("node:fs");
 const parser = new xml.XMLParser();
+const builder = new xml.XMLBuilder();
 fs.readFile("./data.xml", "utf8", (err, xmldata) => {
     if (err) {
         console.log("File read failed", err);
         return;
     }
     const obj = parser.parse(xmldata);
-    const necessaryData = obj.map(item => {
+    const necessaryData = obj.indicators.inflation.map(item => {
         const container = {};
-        if(item.ku==="13" && item.value >5){
-            container.value = item.value;
+        if(item.ku === 13 && item.value >5){
+            container.value=item.value;
         }
         return container;
     })
-    console.log(necessaryData);
+    const filteredData = necessaryData.filter(item => Object.keys(item).length !== 0);
+    const xmlstring = builder.build(filteredData);
+    fs.writeFile("output.xml", xmlstring, (err) => {
+        if (err) {
+            console.log("File write failed",err);
+            return;
+        }
+    })
 });
