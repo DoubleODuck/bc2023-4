eee01ebeffc8a945cad15d02a9b692bad092b3de
Mykola <nikomyk79@gmail.com>
2023-10-16 22:40:28 +0300

    setup the server and made it work

--- main.js
+++ main.js
@@ -4,18 +4,20 @@ const fs = require("node:fs");
 const parser = new xml.XMLParser();
 const builder = new xml.XMLBuilder({ format: true });
 const server = http.createServer((req, res) => {
-        const xmldata = fs.readFile("data.xml","utf-8");
-        const obj = parser.parse(xmldata);
-        const necessaryData = obj.indicators.inflation.map(item => {
-            if (item.ku === 13 && item.value > 5) {
-                return item.value;
-            }
-        })
-        const filteredData = necessaryData.filter(item => item !== undefined);
-        const xmlObj = {
-            data: {
-                value: filteredData
-            }
-        };
-        const xmlstring = builder.build(xmlObj);
-})
+    const xmldata = fs.readFileSync("data.xml", "utf-8");
+    const obj = parser.parse(xmldata);
+    const filteredData = obj.indicators.inflation.filter(item => item.ku === 13 && item.value > 5);
+    const necessaryData = filteredData.map(item => item.value);
+    const xmlObj = {
+        data: {
+            value: necessaryData
+        }
+    };
+    const ActualXml = builder.build(xmlObj);
+    res.setHeader('Content-Type', 'application/xml');
+    res.end(ActualXml);
+});
+const port = 8000;
+server.listen(port, () => {
+    console.log(`Сервер працює на порту ${port}`);
+});
